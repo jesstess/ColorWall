4c1b885b3cfa42fc5fa65e9c6a61811e89caecf8
Jessica McKellar <jesstess@mit.edu>
2011-03-06 19:28:30 -0500

    Add a Letters effect that exercises an 8x8 bit ASCII library.

--- effects.py
+++ effects.py
@@ -1,5 +1,6 @@
 import random
 import time
+import ascii8x8
 
 # A dictionary of hsv values for some common colors.
 colors = {"black":(0, 0, 0), "white":(0, 0, 1), "gray":(0, 0, 0.5), \
@@ -303,4 +304,41 @@ class Matrix(Effect):
                 timeout -= 1
             drawing = 0
 
-Effects = [HueTest, SaturationTest, ValueTest, DictionaryTest, Checkerboards, Columns, Rainbow, Twinkle, KnightMoves, Matrix]
+class LetterTest(Effect):
+    """
+    Cycle through the letters of the alphabet.
+
+    Minimum wall size: 8 x 8.
+    """
+    def run(self):
+        if self.wall.width < 8 or self.wall.height < 8:
+            return
+
+        color = random.random()
+        foreground = (color, 1, 1)
+        # Make the foreground and background complementary colors.
+        background = ((color + .5) % 1, 1, 1)
+
+        # Center the letters on the wall
+        x_offset = int((self.wall.width - 8) / 2)
+        y_offset = int((self.wall.height - 8) / 2)
+
+        # Display upper and lower case letters. The break between 90 and 97 is
+        # for non-letter keyboard characters.
+        for ord in range(65, 91) + range(97, 123):
+            self.wall.clear()
+
+            # Set every pixel to the background color, since ascii8x8 will only
+            # color an 8x8 section.
+            for x in range(self.wall.width):
+                for y in range(self.wall.height):
+                    self.wall.set_pixel(x, y, background)
+
+            # Color the letter.
+            ascii8x8.draw_chr(chr(ord), self.wall, foreground, background,
+                              x_offset, y_offset)
+            self.wall.draw()
+            time.sleep(.1)
+
+Effects = [HueTest, SaturationTest, ValueTest, DictionaryTest, LetterTest,
+           Checkerboards, Columns, Rainbow, Twinkle, KnightMoves, Matrix]
