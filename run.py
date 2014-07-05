54106a00eb449fd1e672e3faf452f90680b7cbff
Jessica McKellar <jesstess@mit.edu>
2014-07-05 15:22:50 -0700

    Switch from deprecated optparse to argparse.
    Thanks Eeshan Garg for the patch!

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import optparse, sys
+import argparse, sys
 
 from wall import Wall
 import effects
@@ -9,20 +9,24 @@ import effects
 """
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser("""Usage: %prog [options]""")
-    parser.add_option("-w", "--width", type="int",
-                      action="store", dest="width",
-                      default=8, help="wall width")
-    parser.add_option("-t", "--height", type="int",
-                      action="store", dest="height",
-                      default=8, help="wall height")
-    (opts, args) = parser.parse_args(sys.argv[1:])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-w", "--width", type=int,
+                        action="store", dest="width",
+                        default=8, help="wall width")
+    parser.add_argument("-t", "--height", type=int,
+                        action="store", dest="height",
+                        default=8, help="wall height")
+    parser.add_argument("-e", "--effects",
+                        nargs='+', dest="effects",
+                        help="specific effects you wish to run")
+    args = parser.parse_args()
 
-    wall = Wall(opts.width, opts.height)
+    wall = Wall(args.width, args.height)
 
-    if args:
-        effects_to_run = [getattr(effects, a) for a in args \
+    if args.effects:
+        effects_to_run = [getattr(effects, a) for a in args.effects \
                               if hasattr(effects, a)]
+
     else:
         effects_to_run = effects.Effects
 
